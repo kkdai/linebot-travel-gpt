98e8efbcc3633c5a7ad85471c95ce0cbc468d99e
Evan Lin <evan.if.lin@linecorp.com>
2023-05-15 17:09:07 +0800

    chre: Remove some unuse event.

--- bot.go
+++ bot.go
@@ -41,35 +41,14 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 			// Handle only on text message
 			case *linebot.TextMessage:
+				if isGroupEvent(event) {
+					// 如果聊天機器人在群組中，不回覆訊息。
+					return
+				}
+
 				// Directly to ChatGPT
 				if strings.Contains(message.Text, ":gpt") {
 					handleGPT(GPT_Complete, event, message.Text)
-				} else if strings.Contains(message.Text, ":draw") {
-					handleGPT(GPT_Draw, event, message.Text)
-				} else if isGroupEvent(event) {
-					// 如果聊天機器人在群組中，開始儲存訊息。
-					//
-				}
-
-			// Handle only on Sticker message
-			case *linebot.StickerMessage:
-				var kw string
-				for _, k := range message.Keywords {
-					kw = kw + "," + k
-				}
-
-				log.Println("Sticker: PID=", message.PackageID, " SID=", message.StickerID)
-
-				if isGroupEvent(event) {
-					// 在群組中，一樣紀錄起來不回覆。
-					// outStickerResult := fmt.Sprintf("貼圖訊息: %s ", kw)
-				} else {
-					// outStickerResult := fmt.Sprintf("貼圖訊息: %s, pkg: %s kw: %s  text: %s", message.StickerID, message.PackageID, kw, message.Text)
-
-					// 1 on 1 就回覆
-					// if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(outStickerResult)).Do(); err != nil {
-					// 	log.Print(err)
-					// }
 				}
 			}
 		}
